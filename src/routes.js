f1ae770a1bab1de9b7ab4e36dd729fc4451b08fc
ssmendes <stellasiqueira17@gmail.com>
2024-09-25 07:11:18 -0300

    fixed some conflicts

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage/index'
-import RepositoriesPage from './pages/RepositoriesPage';
+import RepositoriesPage from './pages/RepositoriesPage/index';
 
 export default function AppRoutes(){
     return (
